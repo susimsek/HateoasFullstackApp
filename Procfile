3a657ced646d97672d4d541d3862f9797e9201d8
suayb <12345678>
2022-03-03 20:40:29 +0300

    added backend

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JAVA_OPTS -jar target/hateoas-backend-0.0.1-SNAPSHOT.jar -D server.port=$PORT $JAR_OPTS
--- hateoas-frontend/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import capabilityReducer from "./capabilityReducer";
-import errorReducer from "./errorReducer";
-
-export default combineReducers({
-  capability: capabilityReducer,
-  errors: errorReducer
-});
