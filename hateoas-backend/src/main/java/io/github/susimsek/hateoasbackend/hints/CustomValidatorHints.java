4c22eaef06b2ac84577529236934845dfe7ee885
suayb <12345678>
2022-03-12 23:32:13 +0300

    added i18n support

---
+++ hateoas-backend/src/main/java/io/github/susimsek/hateoasbackend/hints/CustomValidatorHints.java
@@ -0,0 +1,18 @@
+package io.github.susimsek.hateoasbackend.hints;
+
+import io.github.susimsek.hateoasbackend.validator.FieldsGreatnessValidator;
+import io.github.susimsek.hateoasbackend.validator.annotation.FieldsGreatness;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.nativex.hint.TypeHint;
+
+@TypeHint(types = {
+        FieldsGreatness.class,
+        FieldsGreatnessValidator.class
+})
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass(name = "org.springframework.nativex.NativeListener")
+public class CustomValidatorHints {
+}
+
+
--- hateoas-backend/src/main/java/io/github/susimsek/hateoasbackend/request/CapabilityCreateRequest.java
+++ hateoas-backend/src/main/java/io/github/susimsek/hateoasbackend/request/CapabilityCreateRequest.java
@@ -1,16 +1,24 @@
 package io.github.susimsek.hateoasbackend.request;
 
 
+import io.github.susimsek.hateoasbackend.validator.annotation.FieldsGreatness;
 import io.swagger.v3.oas.annotations.media.Schema;
-import javax.validation.constraints.NotBlank;
 
+import javax.validation.constraints.*;
+
+@FieldsGreatness(firstFieldName = "numOfDevelopers",
+        secondFieldName = "numOfAvailableDevelopers",
+        message = "{error.constraints.FieldsGreatness.message}")
 public record CapabilityCreateRequest(
         @Schema(description = "Name of the Technology Stack.", example = "JavaEE", required = true)
         @NotBlank(message = "{error.constraints.techStack.NotBlank.message}")
+        @Size(min = 1, max = 255)
         String techStack,
 
         @Schema(description = "Num of the Developers.", example = "20", required = false)
+        @PositiveOrZero
         Integer numOfDevelopers,
 
         @Schema(description = "Num of the Available Developers.", example = "10", required = false)
-        Integer numOfAvailableDevelopers){}
+        @PositiveOrZero
+        Integer numOfAvailableDevelopers) {}
